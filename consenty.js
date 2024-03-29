044b99289ecfb84cceba8cb20858d92a862e9838
BernhardBaumrock <office@baumrock.com>
2024-03-29 22:57:45 +0100

    add youtube examples

--- consenty.js
+++ consenty.js
@@ -2,9 +2,18 @@ var consenty;
 (() => {
   class Consenty {
     elements = {};
+    loadProps = {};
 
     allow(prop) {
-      this.save(prop, true);
+      this.change(prop, true);
+    }
+
+    click(selector) {
+      console.log(selector);
+      document.querySelectorAll(selector).forEach((element) => {
+        console.log(element);
+        element.click();
+      });
     }
 
     getStorage() {
@@ -41,12 +50,20 @@ var consenty;
         // clicked on element with revoke attribute
         const prop = el.getAttribute("consenty-revoke");
         this.revoke(prop);
+      } else if ((el = e.target.closest("[consenty-show]"))) {
+        // clicked on element with show attribute
+        const prop = el.getAttribute("consenty-show");
+        this.load(prop);
       }
-    }
 
-    hasConsent(name) {
-      let storage = this.getStorage();
-      return storage && name in storage ? !!storage[name] : false;
+      // trigger consenty:click event if [consenty-click] was clicked
+      if ((el = e.target.closest("[consenty-click]"))) {
+        const prop = el.getAttribute("consenty-click");
+        document.dispatchEvent(
+          new CustomEvent("consenty:click", { detail: prop })
+        );
+        e.preventDefault();
+      }
     }
 
     hideElement(id) {
@@ -59,16 +76,22 @@ var consenty;
     }
 
     init() {
+      document.dispatchEvent(new CustomEvent("consenty:beforeinit"));
       this.reload();
       document.dispatchEvent(new CustomEvent("consenty:init"));
     }
 
     initCheckboxes() {
       document.querySelectorAll("[consenty-toggle]").forEach((checkbox) => {
-        checkbox.checked = this.hasConsent(
+        checkbox.checked = this.isAllowed(
           checkbox.getAttribute("consenty-toggle")
         );
       });
+    }
+
+    isAllowed(name) {
+      let storage = this.getStorage();
+      return storage && name in storage ? !!storage[name] : false;
     }
 
     reload() {
@@ -77,18 +100,33 @@ var consenty;
     }
 
     revoke(prop) {
-      this.save(prop, false);
+      this.change(prop, false);
     }
 
-    save(prop, value) {
+    change(prop, value) {
+      document.dispatchEvent(
+        new CustomEvent("consenty:beforechange", {
+          detail: {
+            name: prop,
+            from: this.isAllowed(prop),
+            to: !!value,
+          },
+        })
+      );
       let storage = this.getStorage();
-      const from = this.hasConsent(prop);
+      const from = this.isAllowed(prop);
       if (!value) {
+        document.dispatchEvent(
+          new CustomEvent("consenty:beforerevoke", { detail: prop })
+        );
         delete storage[prop];
         document.dispatchEvent(
           new CustomEvent("consenty:revoke", { detail: prop })
         );
       } else {
+        document.dispatchEvent(
+          new CustomEvent("consenty:beforeallow", { detail: prop })
+        );
         storage[prop] = 1;
         document.dispatchEvent(
           new CustomEvent("consenty:allow", { detail: prop })
@@ -100,11 +138,11 @@ var consenty;
           detail: {
             name: prop,
             from: from,
-            to: this.hasConsent(prop),
+            to: this.isAllowed(prop),
           },
         })
       );
-      this.toggleIFs();
+      this.reload();
     }
 
     saveStorage(storage) {
@@ -113,45 +151,70 @@ var consenty;
 
     toggleIFs() {
       document.querySelectorAll("[consenty-if]").forEach((el) => {
+        // get property name and type
+        const rawprop = el.getAttribute("consenty-if");
+        const type = rawprop.startsWith("!") ? "hideif" : "showif";
+        let prop = type === "showif" ? rawprop : rawprop.substring(1);
+        const show = this.loadProps.hasOwnProperty(prop);
+
         // get or set id of this element for later use
         let id;
         if (!el.hasAttribute("data-consenty-id")) {
           id = Object.keys(this.elements).length + 1;
-          this.elements[id] = { old: el, new: false };
+          this.elements[id] = { old: el, new: false, prop };
           el.setAttribute("data-consenty-id", id);
         } else {
           id = el.getAttribute("data-consenty-id");
         }
 
-        // get property name and type
-        const rawprop = el.getAttribute("consenty-if");
-        const type = rawprop.startsWith("!") ? "hideif" : "showif";
-        let prop = type === "showif" ? rawprop : rawprop.substring(1);
-
         // toggle element / code
         if (type === "showif") {
-          if (this.hasConsent(prop)) this.showElement(id);
+          if (show || this.isAllowed(prop)) this.loadElement(id);
           else this.hideElement(id);
         } else {
-          if (this.hasConsent(prop)) this.hideElement(id);
-          else this.showElement(id);
+          if (show || this.isAllowed(prop)) this.hideElement(id);
+          else this.loadElement(id);
         }
       });
     }
 
-    showElement(id) {
+    load(prop) {
+      return new Promise((resolve) => {
+        this.loadProps[prop] = true;
+        this.reload();
+        resolve();
+      });
+    }
+
+    loadElement(id) {
       const element = this.elements[id];
       if (!element.new) {
         let tagName = element.old.tagName.toLowerCase();
-        if (tagName === "template") {
-          let newDiv = document.createElement("div");
-          let el = element.old;
-          el.parentNode.insertBefore(newDiv, el.nextSibling);
-          newDiv.appendChild(document.importNode(el.content, true));
-          element.new = newDiv;
-          this.elements[id] = element;
-          this.reload();
-        }
+        if (tagName !== "template") return;
+
+        document.dispatchEvent(
+          new CustomEvent("consenty:beforeload", {
+            detail: id,
+          })
+        );
+
+        let el = element.old;
+        let newDiv = document.createElement(
+          el.getAttribute("consenty-tag") === "span" ? "span" : "div"
+        );
+        el.parentNode.insertBefore(newDiv, el.nextSibling);
+        newDiv.appendChild(document.importNode(el.content, true));
+        element.new = newDiv;
+        this.elements[id] = element;
+
+        document.dispatchEvent(
+          new CustomEvent("consenty:load", {
+            detail: id,
+          })
+        );
+
+        // this will make it work recursively
+        this.reload();
       }
     }
   }
